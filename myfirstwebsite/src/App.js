7771a7fc1e7ea5b4d5589aaff11b65b5923b0cb6
nrttinkplan <erhannurettinkplan14@gmail.com>
2023-07-11 02:11:32 +0300

    react1

---
+++ myfirstwebsite/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <h1>Logo</h1>
+        <ul>
+          <li>Ana Sayfa</li>
+          <li>Proje</li>
+          <li>Hakkımızda</li>
+          <li>İletişim</li>
+        </ul>
+      </nav>
+      <section className="backimage"></section>
+    </>
+  );
+}
+
+export default App;
